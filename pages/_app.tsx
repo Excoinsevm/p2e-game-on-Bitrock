aeb1d7d2b54a06286e691df27527866d97b092f3
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2023-12-31 03:27:11 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from "next/app";
-import { Bitrock } from "@thirdweb-dev/chains";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
@@ -11,7 +10,27 @@ import NavBar from "../components/NavBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={ Bitrock }>
+    <ThirdwebProvider
+      activeChain={{
+        // === Required information for connecting to the network === \\
+        chainId: 7171, // Chain ID of the network
+        // Array of RPC URLs to use
+        rpc: ["https://bitrock.rpc.thirdweb.com"],
+
+        // === Information for adding the network to your wallet (how it will appear for first time users) === \\
+        // Information about the chain's native currency (i.e. the currency that is used to pay for gas)
+        nativeCurrency: {
+          decimals: 18,
+          name: "Bitrock",
+          symbol: "BROCK",
+        },
+        shortName: "Brock", // Display value shown in the wallet UI
+        slug: "Bitrock", // Display value shown in the wallet UI
+        testnet: false, // Boolean indicating whether the chain is a testnet or mainnet
+        chain: "Bitrock", // Name of the network
+        name: "Bitrock Mainnet", // Name of the network
+      }}
+    >
       <ChakraProvider>
         <NavBar/>
         <Component {...pageProps} />
